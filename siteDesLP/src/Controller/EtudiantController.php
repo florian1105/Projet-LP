2868f7fc4cd3742e5186792140770c080d2cce91
Théo AUGUST <theoaugust@macbook-pro-de-theo.home>
2019-11-03 00:04:15 +0100

    Modification d'un étudiant dans la même fonction v2

--- siteDesLP/src/Controller/EtudiantController.php
+++ siteDesLP/src/Controller/EtudiantController.php
@@ -18,11 +18,12 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 class EtudiantController extends AbstractController
 {
   /**
-  * @Route("/etudiant_create", name="create")
+  * @Route("/etudiant/new", name="etudiant_create")
+  * @Route("/etudiant/{id}/edit", name="etudiant_edit")
   */
-  public function create(Request $request, ObjectManager $manager, UserPasswordEncoderInterface $encoder)
+  public function form(Etudiants $etudiant = null, Request $request, ObjectManager $manager, UserPasswordEncoderInterface $encoder)
   {
-    $etudiant = new Etudiants();
+    if(!$etudiant) $etudiant = new Etudiants();
 
     $form = $this->createFormBuilder($etudiant)
     ->add('nomEtudiant')
@@ -44,97 +45,36 @@ class EtudiantController extends AbstractController
     // $login = strtolower($form['nomEtudiant']->getData()).$prenom1;
     // $etudiant->setLogin($login);
 
-    $hash = $encoder->encodePassword($etudiant, $etudiant->getPassword());
-    $etudiant->setPassword($hash);
+    if(!$etudiant)
+    {
+      $hash = $encoder->encodePassword($etudiant, $etudiant->getPassword());
+      $etudiant->setPassword($hash);
+    }
 
     if($form->isSubmitted() && $form->isValid())
     {
       $manager->persist($etudiant);
       $manager->flush();
 
+      if($etudiant) return $this->redirectToRoute('list');
 
-      $etudiant_new = new Etudiants();
-      $form = $this->createFormBuilder($etudiant_new)
-      ->add('nomEtudiant')
-      ->add('prenomEtudiant')
-      ->add('login')
-      ->add('password', PasswordType::class)
-      ->add('mail')
-      ->add('mailAcademique')
-      ->add('dateNaissance', DateType::class, [
-        'widget' => 'single_text'
-      ])
-
-    ->getForm();
-  }
+    }
     return $this->render('etudiant/index.html.twig', [
       'form_create_etudiant' => $form->createView(),
+      'editMode' => $etudiant->getId() !== null,
+      'etudiant' => $etudiant,
     ]);
   }
 
   /**
-  * @Route("/etudiant_edit", name="edit")
+  * @Route("etudiant/etudiant_list", name="list")
   */
   public function showEtudiants(EtudiantsRepository $repo)
   {
     $etudiants = $repo->findAll();
-    return $this->render('etudiant/edit.html.twig', [
+    return $this->render('etudiant/list.html.twig', [
       'etudiants' => $etudiants,
     ]);
   }
 
-  /**
-  * @Route("/etudiant_edit/{id}", name="edit_show")
-  */
-  public function edit(EtudiantsRepository $repo, Request $request, $id)
-  {
-    $etudiant = $repo->find($id);
-
-    $form = $this->createFormBuilder($etudiant)
-    ->add('nomEtudiant', TextType::class, ['label' => 'Nom étudiant'])
-    ->add('prenomEtudiant', TextType::class, ['label' => 'Prénom étudiant'])
-    ->add('mail', TextType::class, ['label' => 'E-mail'])
-    ->add('mailAcademique', TextType::class, ['label' => 'E-mail académique'])
-    ->add('login')
-    ->add('password', PasswordType::class, ['label' => 'Mot de passe'])
-    ->add('dateNaissance', DateType::class, [
-      'widget' => 'single_text',
-      'label' => 'Date de naissance'
-    ])
-
-    ->getForm();
-
-    $form->handleRequest($request);
-
-    if($form->isSubmitted() && $form->isValid())
-    {
-      $prenom = strtolower($form['prenomEtudiant']->getData());
-      $prenom1 = substr($prenom, 0,1);
-      $login = strtolower($form['nomEtudiant']->getData()).$prenom1;
-      $etudiant->setLogin($login);
-      $manager->persist($etudiant);
-      $manager->flush();
-
-      
-      $form = $this->createFormBuilder($etudiant)
-      ->add('nomEtudiant')
-      ->add('prenomEtudiant')
-      ->add('password', PasswordType::class)
-      ->add('mail')
-      ->add('mailAcademique')
-      ->add('dateNaissance', DateType::class, [
-        'widget' => 'single_text'
-      ])
-
-      ->getForm();
-
-
-    }
-
-
-    return $this->render('etudiant/show.html.twig', [
-      'etudiant' => $etudiant,
-      'form_create_etudiant' => $form->createView(),
-    ]);
-  }
 }
