16f22d4772d8d7f98474a66a087abd21bf3db18f
Théo AUGUST <theoaugust@MacBook-Pro-de-Theo.local>
2019-11-06 15:21:27 +0100

    Génération automatique des logins avec gestion des doublons

--- siteDesLP/src/Controller/EtudiantController.php
+++ siteDesLP/src/Controller/EtudiantController.php
@@ -22,31 +22,65 @@ class EtudiantController extends AbstractController
   * @Route("/etudiant/new", name="etudiant_create")
   * @Route("/etudiant/{id}/edit", name="etudiant_edit")
   */
-  public function form(Etudiants $etudiant = null, Request $request, ObjectManager $manager, UserPasswordEncoderInterface $encoder)
+  public function form(Etudiants $etudiant = null, Etudiantsrepository $repo, Request $request, ObjectManager $manager, UserPasswordEncoderInterface $encoder)
   {
-    if(!$etudiant) $etudiant = new Etudiants();
+    $editMode = true;
 
-    $form = $this->createFormBuilder($etudiant)
-    ->add('nomEtudiant')
-    ->add('prenomEtudiant')
-    ->add('login')
-    ->add('password', PasswordType::class)
-    ->add('mail')
-    ->add('mailAcademique')
-    ->add('dateNaissance', DateType::class, [
-      'widget' => 'single_text'
-    ])
+    if(!$etudiant) 
+    {
+      $etudiant = new Etudiants();
+      $editMode = false;
+    }
 
-    ->getForm();
+    if($editMode == false)
+    {
+      $form = $this->createFormBuilder($etudiant)
+      ->add('nomEtudiant')
+      ->add('prenomEtudiant')
+      ->add('password', PasswordType::class)
+      ->add('mail')
+      ->add('mailAcademique')
+      ->add('dateNaissance', DateType::class, [
+        'widget' => 'single_text'
+      ])
 
-    $form->handleRequest($request);
+      ->getForm();
 
-    // $prenom = strtolower($form['prenomEtudiant']->getData());
-    // $prenom1 = substr($prenom, 0,1);
-    // $login = strtolower($form['nomEtudiant']->getData()).$prenom1;
-    // $etudiant->setLogin($login);
+      $form->handleRequest($request);
 
-    if(!$etudiant)
+      $prenom = strtolower($form['prenomEtudiant']->getData());
+      $prenom1 = substr($prenom, 0,1);
+      $login = strtolower($form['nomEtudiant']->getData()).$prenom1;
+      
+      $i = "";
+
+      while($repo->findBy(['login' => $login.$i]))
+      {
+        if($i == "") $i = 1;
+        $i++;
+      }
+
+      $etudiant->setLogin($login.$i);
+
+    }
+    else
+    {
+      $form = $this->createFormBuilder($etudiant)
+      ->add('nomEtudiant')
+      ->add('prenomEtudiant')
+      ->add('login')
+      ->add('mail')
+      ->add('mailAcademique')
+      ->add('dateNaissance', DateType::class, [
+        'widget' => 'single_text'
+      ])
+
+      ->getForm();
+    }
+
+    //$form->handleRequest($request);
+
+    if($editMode == false)
     {
       $hash = $encoder->encodePassword($etudiant, $etudiant->getPassword());
       $etudiant->setPassword($hash);
@@ -57,7 +91,7 @@ class EtudiantController extends AbstractController
       $manager->persist($etudiant);
       $manager->flush();
 
-      if($etudiant) return $this->redirectToRoute('list');
+      return $this->redirectToRoute('list');
 
     }
     return $this->render('etudiant/index.html.twig', [
