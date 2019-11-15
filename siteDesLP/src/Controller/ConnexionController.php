70128e7d5f372f5c173cdf029197ff280f3395ee
Théo AUGUST <theoaugust@MacBook-Pro-de-Theo.local>
2019-11-15 22:44:41 +0100

    Professeurs et login v1

---
+++ siteDesLP/src/Controller/ConnexionController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ConnexionController extends AbstractController
+{
+    /**
+     * @Route("/connexion", name="connexion")
+     */
+    public function index()
+    {
+        return $this->render('connexion/index.html.twig');
+    }
+}
---
+++ siteDesLP/src/Entity/Professeurs.php
@@ -0,0 +1,186 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProfesseursRepository")
+ */
+class Professeurs
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=64)
+     */
+    private $nomProfesseur;
+
+    /**
+     * @ORM\Column(type="string", length=64)
+     */
+    private $prenomProfesseur;
+
+    /**
+     * @ORM\Column(type="string", length=64)
+     */
+    private $mailAcademique;
+
+    /**
+     * @ORM\Column(type="string", length=64)
+     */
+    private $login;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $dateNaissance;
+
+    /**
+     * @ORM\Column(type="string", length=64)
+     */
+    private $password;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\Classes", inversedBy="professeurs")
+     */
+    private $classes;
+
+    /**
+     * @ORM\OneToOne(targetEntity="App\Entity\Classes", mappedBy="professeurResponsable", cascade={"persist", "remove"})
+     */
+    private $classeResponsable;
+
+    public function __construct()
+    {
+        $this->classes = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomProfesseur(): ?string
+    {
+        return $this->nomProfesseur;
+    }
+
+    public function setNomProfesseur(string $nomProfesseur): self
+    {
+        $this->nomProfesseur = $nomProfesseur;
+
+        return $this;
+    }
+
+    public function getPrenomProfesseur(): ?string
+    {
+        return $this->prenomProfesseur;
+    }
+
+    public function setPrenomProfesseur(string $prenomProfesseur): self
+    {
+        $this->prenomProfesseur = $prenomProfesseur;
+
+        return $this;
+    }
+
+    public function getMailAcademique(): ?string
+    {
+        return $this->mailAcademique;
+    }
+
+    public function setMailAcademique(string $mailAcademique): self
+    {
+        $this->mailAcademique = $mailAcademique;
+
+        return $this;
+    }
+
+    public function getLogin(): ?string
+    {
+        return $this->login;
+    }
+
+    public function setLogin(string $login): self
+    {
+        $this->login = $login;
+
+        return $this;
+    }
+
+    public function getDateNaissance(): ?\DateTimeInterface
+    {
+        return $this->dateNaissance;
+    }
+
+    public function setDateNaissance(\DateTimeInterface $dateNaissance): self
+    {
+        $this->dateNaissance = $dateNaissance;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Classes[]
+     */
+    public function getClasses(): Collection
+    {
+        return $this->classes;
+    }
+
+    public function addClass(Classes $class): self
+    {
+        if (!$this->classes->contains($class)) {
+            $this->classes[] = $class;
+        }
+
+        return $this;
+    }
+
+    public function removeClass(Classes $class): self
+    {
+        if ($this->classes->contains($class)) {
+            $this->classes->removeElement($class);
+        }
+
+        return $this;
+    }
+
+    public function getClasseResponsable(): ?Classes
+    {
+        return $this->classeResponsable;
+    }
+
+    public function setClasseResponsable(?Classes $classeResponsable): self
+    {
+        $this->classeResponsable = $classeResponsable;
+
+        // set (or unset) the owning side of the relation if necessary
+        $newProfesseurResponsable = null === $classeResponsable ? null : $this;
+        if ($classeResponsable->getProfesseurResponsable() !== $newProfesseurResponsable) {
+            $classeResponsable->setProfesseurResponsable($newProfesseurResponsable);
+        }
+
+        return $this;
+    }
+}
