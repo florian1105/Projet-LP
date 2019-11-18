c25444a6d507b82e4eb83f6630246ac295714197
Lucas <Lucas@MacBook-Pro-de-Lucas.local>
2019-11-19 00:03:56 +0100

    Affinement des assert sur les champs des formulaires

--- siteDesLP/src/Entity/Classes.php
+++ siteDesLP/src/Entity/Classes.php
@@ -23,11 +23,12 @@ class Classes
 
     /**
      * @ORM\Column(type="string", length=32)
+      * @Assert\NotBlank(message="Veuillez renseigner un nom pour cette classe")
      * @Assert\Regex(pattern="/^-/", match=false, message="les - ne sont pas autorisés au début.")
      * @Assert\Regex(pattern="/-$/", match=false, message="les - ne sont pas autorisés a la fin.")
      * @Assert\Regex(pattern="/^[[:blank:]]/", match=false, message="les espaces ne sont pas autorisés au début.")
      * @Assert\Regex(pattern="/[[:blank:]]$/", match=false, message="les espaces ne sont pas autorisés a la fin")
-     * @Assert\Regex(pattern="/[☺☻♥♦♣♠•◘○◙♂♀♪♫☼►◄↕‼¶§▬↨↑↓→←∟↔▲▼]/", match=false, message="les caractéres spéciaux ne sont pas autorisés")
+     * @Assert\Regex(pattern="/[☺☻♥♦♣♠•◘○◙♂♀♪♫☼►◄↕‼¶§▬↨↑↓→←∟↔▲▼&{}*$£%¨^%+=.;,?\\']/", match=false, message="les caractéres spéciaux ne sont pas autorisés")
      */
     private $nomClasse;
 
