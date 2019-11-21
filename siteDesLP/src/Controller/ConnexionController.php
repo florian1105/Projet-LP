59dea72cdc55d4d9680b1f159ba1fe8973aa2103
Lucas <Lucas@MacBook-Pro-de-Lucas.local>
2019-11-21 15:57:13 +0100

    Ajout d'un message si les logins sont incorrects

--- siteDesLP/src/Controller/ConnexionController.php
+++ siteDesLP/src/Controller/ConnexionController.php
@@ -4,15 +4,19 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class ConnexionController extends AbstractController
 {
     /**
      * @Route("/connexion", name="connexion")
      */
-    public function connexion()
+    public function connexion(AuthenticationUtils $authUtils)
     {
-        return $this->render('connexion/index.html.twig');
+      $error = $authUtils->getLastAuthenticationError();
+        return $this->render('connexion/index.html.twig' , [
+          'error' => $error
+        ]);
     }
 
     /**
