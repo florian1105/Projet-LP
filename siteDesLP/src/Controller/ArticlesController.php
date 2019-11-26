6514a8406fc545062870a065054b3919d48968f7
Lucas <Lucas@MacBook-Pro-de-Lucas.local>
2019-11-26 02:03:17 +0100

    Création d'article v1

--- siteDesLP/src/Controller/ArticlesController.php
+++ siteDesLP/src/Controller/ArticlesController.php
@@ -5,7 +5,11 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use FOS\CKEditorBundle\Form\Type\CKEditorType;
+use App\Entity\Classes;
+use App\Entity\Articles;
+use Doctrine\Common\Persistence\ObjectManager;
 
 
 
@@ -15,20 +19,41 @@ class ArticlesController extends AbstractController
     /**
      * @Route("/articles", name="articles")
      */
-    public function create()
+    public function create(Request $request, ObjectManager $em)
     {
-      $form = $this->createFormBuilder()
-        ->add('content', CKEditorType::class, [
+      $article = new Articles();
+      $form = $this->createFormBuilder($article)
+        ->add('titre')
+        ->add('description', CKEditorType::class, [
           'config' => [
             'uiColor' => '#e2e2e2',
-            'toolabar' => 'full',
+            'toolbar' => 'full',
             'required' => 'true'
           ]
         ])
+        ->add('classes', EntityType::class, [
+          'class' => Classes::class,
+					'choice_label' => 'nomClasse',
+          'label' => "Classe(s) concernées par l'article",
+					'expanded' => true,
+					'multiple' => true,
+					'mapped' => false, //décoché par défaut
+					'by_reference' => false,
+        ])
         ->getForm();
 
+        $form->handleRequest($request);
+        $article->setDate(new \DateTime());
+        if($form->isSubmitted() && $form->isValid())
+        {
+          $em->persist($article);
+          $em->flush();
+        }
+        dump($article);
+        $this->redirectToRoute('connexion');
+
         return $this->render('articles/index.html.twig', [
-            'form' => $form->createView()
+            'form_article' => $form->createView()
         ]);
     }
 }
