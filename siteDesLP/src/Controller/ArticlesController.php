14a46686e133b98373696bbf6cbd3c2f2f1f1e08
Lucas <Lucas@MacBook-Pro-de-Lucas.local>
2019-11-27 15:55:06 +0100

    Gestion des articles Suppressions des articles ok Modification des articles non fonctionnel Ajout des articles idem

--- siteDesLP/src/Controller/ArticlesController.php
+++ siteDesLP/src/Controller/ArticlesController.php
@@ -17,45 +17,130 @@ use Doctrine\Common\Persistence\ObjectManager;
 
 class ArticlesController extends AbstractController
 {
-    /**
-     * @Route("/article", name="article")
-     */
-    public function create(Request $request, ObjectManager $em)
+  /**
+  * @Route("/article_add", name="article_add")
+  * @Route("/article/edit/{id}", name="article_edit")
+  */
+  public function form(Articles $article = null,Request $request, ObjectManager $em)
+  {
+    if(!$article)
     {
-      $article = new Articles();
-      $form = $this->createFormBuilder($article)
-        ->add('titre')
-        ->add('description', CKEditorType::class, [
-          'config' => [
-            'uiColor' => '#e2e2e2',
-            'toolbar' => 'full',
-            'required' => 'true'
-          ]
-        ])
-        ->add('classes', EntityType::class, [
-          'class' => Classes::class,
-					'choice_label' => 'nomClasse',
-          'label' => "Classe(s) concernées par l'article",
-					'expanded' => true,
-					'multiple' => true,
-					'mapped' => false, //décoché par défaut
-					'by_reference' => false,
-        ])
-        ->getForm();
+    $article = new Articles();
+    $form = $this->createFormBuilder($article)
+    ->add('titre')
+    ->add('description', CKEditorType::class, [
+      'config' => [
+        'uiColor' => '#e2e2e2',
+        'toolbar' => 'full',
+        'required' => 'true'
+      ]
+    ])
+    ->add('classes', EntityType::class, [
+      'class' => Classes::class,
+      'choice_label' => 'nomClasse',
+      'label' => "Classe(s) concernées par l'article",
+      'expanded' => true,
+      'multiple' => true,
+      'mapped' => false, //décoché par défaut
+      'by_reference' => false,
+    ])
+    ->getForm();
 
-        $form->handleRequest($request);
-        $article->setDate(new \DateTime());
+    $form->handleRequest($request);
+    $article->setDate(new \DateTime());
 
-
-        if($form->isSubmitted() && $form->isValid())
-        {
-
-          $em->persist($article);
-          $em->flush();
-        }
-
-        return $this->render('articles/index.html.twig', [
-            'form_article' => $form->createView()
-        ]);
+    if($form->isSubmitted() && $form->isValid())
+    {
+      $em->persist($article);
+      $em->flush();
+      return $this->redirectToRoute('article_search');
     }
+
+
+
+  }
+  //Edit mode ON
+  else {
+    $form = $this->createFormBuilder($article)
+    ->add('titre')
+    ->add('description', CKEditorType::class, [
+      'config' => [
+        'uiColor' => '#e2e2e2',
+        'toolbar' => 'full',
+        'required' => 'true'
+      ]
+    ])
+    ->add('classes', EntityType::class, [
+      'class' => Classes::class,
+      'choice_label' => 'nomClasse',
+      'label' => "Classe(s) concernées par l'article",
+      'expanded' => true,
+      'multiple' => true,
+      'mapped' => true, //décoché par défaut
+    ])
+    ->getForm();
+
+    if($form->isSubmitted() && $form->isValid())
+    {
+      $em->persist($article);
+      $em->flush();
+      return $this->redirectToRoute('articles');
+    }
+
+  }
+
+
+
+    return $this->render('articles/index.html.twig', [
+      'form_article' => $form->createView(),
+      'editMode' => $article->getId() !== null,
+    ]);
+  }
+
+  /**
+  * @Route("/articles", name="article_search")
+  */
+  public function research(ArticlesRepository $repoA)
+  {
+    $articles = $repoA->findAll();
+    return $this->render('articles/research.html.twig', [
+      'articles' => $articles
+    ]);
+
+  }
+
+  /**
+  * @Route("/article/remove/{id}", name="article_delete")
+  */
+  public function delete(Articles $article, Request $request, ObjectManager $em)
+  {
+
+    //Si le formulaire à été soumis
+    if($request->isMethod('POST'))
+    {
+      // En cas de validation on supprime et on redirige
+      if($request->request->has('oui'))
+      {
+          $em->remove($article);
+          $em->flush();
+          $this->addFlash('delete',"Cet article a été supprimé avec succès");
+          return $this->redirectToRoute('article_search');
+      }
+
+    }
+    else
+    {
+      //Si le formulaire n'a pas été soumis alors on l'affiche
+      $title = 'Êtes-vous sûr(e) de vouloir supprimer cet article ?';
+
+      $message = 'Article n°'.$article->getId().' ayant pour titre : '.
+      $article->getTitre().' datant du '.
+      $article->getDate()->format('Y-m-d');
+
+      return $this->render('confirmation.html.twig', [
+        'titre' => $title,
+        'message' => $message
+      ]);
+    }
+  }
 }
