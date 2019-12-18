7e304f57b924b91c69d88a700a8c015e21f74f7d
Lucas ESCAVIA <l.escavia@gmail.com>
2019-12-18 21:18:32 +0100

    Gestion de la modifications des plaquettes d'un LP

--- siteDesLP/src/Controller/InformationsClassesController.php
+++ siteDesLP/src/Controller/InformationsClassesController.php
@@ -13,113 +13,140 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 
 class InformationsClassesController extends AbstractController
 {
 
-    /**
-     * @Route("/informations/classes", name="informations_classes")
-     */
-    public function afficherLes(ClassesRepository $repoC)
+  /**
+  * @Route("/informations/classes", name="informations_classes")
+  */
+  public function afficherLes(ClassesRepository $repoC)
+  {
+    $classes = $repoC->findAll();
+
+    return $this->render('informations_classes/lesclasses.html.twig', [
+      'classes' => $classes,
+    ]);
+  }
+
+  /**
+  * @Route("/informations/classes/{id}", name="informations_classes_id")
+  */
+  public function afficher(InformationsClasses $info)
+  {
+    return $this->render('informations_classes/info.html.twig', [
+      'info' => $info,
+    ]);
+  }
+
+  /**
+  * @Route("/informations/classes/edit/print", name="informations_classes_print")
+  */
+  public function print(InformationsClassesRepository $repoI)
+  {
+    $classe = $this->getUser()->getClasseResponsable();
+
+    $info = $repoI->findBy(['classe' => $classe->getId()]);
+
+    dump($info);
+
+    return $this->render('informations_classes/print.html.twig', [
+      'info' => $info[0],
+      'classe' => $classe
+    ]);
+  }
+
+  /**
+  * @Security("is_granted('ROLE_PROFESSEURRESPONSABLE')")
+  * @Route("/informations/classes/edit/{id}", name="informations_classes_edit")
+  */
+  public function form(InformationsClasses $info, InformationsClassesRepository $repoI, Request $request, ObjectManager $manager)
+  {
+    $classe = $this->getUser()->getClasseResponsable();
+
+    $idInfo = $repoI->findBy(['classe' => $info->getClasse()]);
+
+    $cheminPlaquette = $info->getCheminPlaquette();
+
+    //Si le professeur est bien responsable de cette classe
+    if($idInfo[0]->getClasse()->getProfesseurResponsable()->getId() == $this->getUser()->getId())
     {
-            $classes = $repoC->findAll();
+      $cheminPlaquette = $info->getCheminPlaquette();
+      $plaquetteDirectory = $this->getParameter('plaquette_directory');
+      $plaquette = $plaquetteDirectory . "/" . $cheminPlaquette;
 
-        return $this->render('informations_classes/lesclasses.html.twig', [
-            'classes' => $classes,
-        ]);
-    }
+      $form = $this->createFormBuilder($info)
+      ->add('description', CKEditorType::class, [
+        'config' => [
+          'uiColor' => '#e2e2e2',
+          'toolabar' => 'full',
+          'required' => 'true'
+          ]])
+          ->add('cheminPlaquette', FileType::class , [
+            'data_class' => null,
+            'mapped' => true,
+            'required' => false,
+            // 'empty_data' => $plaquette,
+          ])
 
-    /**
-     * @Route("/informations/classes/{id}", name="informations_classes_id")
-     */
-    public function afficher(InformationsClasses $info)
-    {
-        return $this->render('informations_classes/info.html.twig', [
-            'info' => $info,
-        ]);
-    }
+          ->getForm();
 
-     /**
-     * @Route("/informations/classes/edit/print", name="informations_classes_print")
-     */
-    public function print(InformationsClassesRepository $repoI)
-    {
-        $classe = $this->getUser()->getClasseResponsable();
+          $form->handleRequest($request);
+          dump($cheminPlaquette);
+          if($form->isSubmitted() && $form->isValid())
+          {
+              if($form['cheminPlaquette']->getData() == null)
+              {
+                if($cheminPlaquette == null)
+                {
+                  $info->setCheminPlaquette(null);
+                }
+                else
+                {
+                  $plaquetteDirectory = $this->getParameter('plaquette_directory');
+                  $plaquette = $plaquetteDirectory . "/" . $cheminPlaquette;
+                  $info->setCheminPlaquette($plaquette);
+                }
 
-        $info = $repoI->findBy(['classe' => $classe->getId()]);
-
-
-        return $this->render('informations_classes/print.html.twig', [
-            'info' => $info[0],
-            'classe' => $classe
-        ]);
-    }
-
-    /**
-     * @Security("is_granted('ROLE_PROFESSEURRESPONSABLE')")
-     * @Route("/informations/classes/edit/{id}", name="informations_classes_edit")
-     */
-    public function form(InformationsClasses $info, InformationsClassesRepository $repoI, Request $request, ObjectManager $manager)
-    {
-        $classe = $this->getUser()->getClasseResponsable();
-
-        $idInfo = $repoI->findBy(['classe' => $info->getClasse()]);
-
-        //Si le professeur est bien responsable de cette classe
-        if($idInfo[0]->getClasse()->getProfesseurResponsable()->getId() == $this->getUser()->getId())
-        {
-          // if($info->getCheminPlaquette())
-          // {
-          //     $info->setCheminPlaquette(new File($this->getParameter('plaquette_directory').'/'.$info->getCheminPlaquette()));
-          // }
-
-            $form = $this->createFormBuilder($info)
-            ->add('description', CKEditorType::class, [
-                'config' => [
-                  'uiColor' => '#e2e2e2',
-                  'toolabar' => 'full',
-                  'required' => 'true'
-                ]])
-            ->add('cheminPlaquette', FileType::class , [
-              'data_class' => null
-            ])
-            ->getForm();
-            $form->handleRequest($request);
-
-
-            if($form->isSubmitted() && $form->isValid())
-            {
-              // $file stores the uploaded PDF file
-                // $file = $form->get('cheminPlaquette')->getData();
+              }
+              else
+              {
                 $file = $info->getCheminPlaquette();
                 $fileName = $this->generateUniqueFileName().'.'.$file->guessExtension();
                 $file->move($this->getParameter('plaquette_directory'),$fileName);
                 $info->setCheminPlaquette($fileName);
-                $manager->persist($info);
-                $manager->flush();
-
-                $this->addFlash('validModificationInformationsClasses',"Les informations ont été modifié avec succès");
-                return $this->redirectToRoute('informations_classes_print');
-            }
+                $info->setCheminPlaquette($info->getCheminPlaquette());
+              }
 
 
-            return $this->render('informations_classes/index.html.twig', [
-                'form' => $form->createView(),
-                'classe' => $classe
-            ]);
+            $manager->persist($info);
+            $manager->flush();
+
+            $this->addFlash('validModificationInformationsClasses',"Les informations ont été modifié avec succès");
+            return $this->redirectToRoute('informations_classes_print');
+          }
+
+
+          return $this->render('informations_classes/index.html.twig', [
+            'form' => $form->createView(),
+            'classe' => $classe
+          ]);
         }
         else //Sinon il n'y a pas accès
         {
-            $this->addFlash('errorModificationInformationsClasses',"Vous n'êtes pas responsable de cette licence");
+          $this->addFlash('errorModificationInformationsClasses',"Vous n'êtes pas responsable de cette licence");
 
-            return $this->redirectToRoute('informations_classes_print');
+          return $this->redirectToRoute('informations_classes_print');
 
         }
-    }
+      }
 
-    private function generateUniqueFileName()
-    {
+      private function generateUniqueFileName()
+      {
         return md5(uniqid());
+      }
+
+      //| replace({'/Applications/MAMP/htdocs/Projet-LP/siteDesLP/public/uploads/plaquette':''})
     }
-}
