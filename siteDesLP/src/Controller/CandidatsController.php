9de659db38e40ba754a50ee3eb1db41ab502e902
Samuel-Quetin <samuel.quetin@etu.umontpellier.fr>
2020-01-09 14:23:18 +0100

    Ajout des Repositorys de Utilisateurs et Candidats
    Signed-off-by: Samuel-Quetin <samuel.quetin@etu.umontpellier.fr>

--- siteDesLP/src/Controller/CandidatsController.php
+++ siteDesLP/src/Controller/CandidatsController.php
@@ -44,8 +44,8 @@ class CandidatsController extends AbstractController
 
 
     /**
-     * @Route("/Candidat/new", name="Candidat_create")
-     * @Route("/Candidat/{id}/edit", name="Candidat_edit")
+     * @Route("/candidats/new", name="candidat_create")
+     * @Route("/candidats/{id}/edit", name="candidat_edit")
      */
     public function form(Candidats $Candidat = null, Candidatsrepository $repoC, Etudiantsrepository $repoE, ProfesseursRepository $repoP, SecretaireRepository $repoS, Request $request, ObjectManager $em, UserPasswordEncoderInterface $encoder)
     {
@@ -116,7 +116,7 @@ class CandidatsController extends AbstractController
                 $em->flush();
 
 
-                return $this->redirectToRoute('research_Candidat');
+                return $this->redirectToRoute('research_candidat');
 
             }
         }
@@ -173,7 +173,7 @@ class CandidatsController extends AbstractController
             {
                 $hash = $encoder->encodePassword($Candidat, $Candidat->getNewPassword());
                 $Candidat->setPassword($hash);
-                $this->addFlash('success','l\'étudiant a bien été créé');
+                $this->addFlash('success','le candidat a bien été créé');
             }
             else{
                 $this->addFlash('success_modifie','les changements on biens été pris en compte');
@@ -182,19 +182,19 @@ class CandidatsController extends AbstractController
             $em->flush();
 
 
-            return $this->redirectToRoute('research_Candidat');
+            return $this->redirectToRoute('research_candidat');
 
         }
 
-        return $this->render('Candidat/index.html.twig', [
-            'form_create_Candidat' => $form->createView(),
+        return $this->render('candidats/index.html.twig', [
+            'form_create_candidat' => $form->createView(),
             'editMode' => $Candidat->getId() !== null,
             'Candidat' => $Candidat,
         ]);
     }
 
     /**
-     * @Route("Candidat/Candidat_delete/{id}", name="Candidat_delete")
+     * @Route("candidats/candidat_delete/{id}", name="candidat_delete")
      */
     public function deleteCandidat(Candidats $etu, Request $req)
     {
@@ -207,11 +207,11 @@ class CandidatsController extends AbstractController
                 $em->flush();
             }
             // Sinon on redirige simplement
-            $this->addFlash('delete','Étudiant supprimé');
+            $this->addFlash('delete','Candidat supprimé');
             return $this->redirectToRoute('research_Candidat');
         } else {
             //Si le formulaire n'a pas été soumis alors on l'affiche
-            $title = 'Êtes-vous sûr(e) de vouloir supprimer cet étudiant ?';
+            $title = 'Êtes-vous sûr(e) de vouloir supprimer ce candidat ?';
 
             $message = 'N°'.$etu->getId().' : '.
                 $etu->getPrenom().' '.
@@ -227,34 +227,30 @@ class CandidatsController extends AbstractController
     }
 
     /**
-     * @Route("Candidat/Candidat_research", name="research_Candidat")
+     * @Route("candidats/candidat_research", name="research_candidat")
      */
     public function researchCandidat(CandidatsRepository $repoE)
     {
-        if($this->getUser()->getRoles()[0] == "ROLE_PROFESSEURRESPONSABLE") //Si l'utilisateur est un professeur responsable
-        {
-            $Candidats = $repoE->findBy(['classeCandidat' => $this->getUser()->getClasseResponsable()]);
-        }
-        else $Candidats = $repoE->findAll();
+        $candidats = $repoE->findAll();
 
-        return $this->render('Candidat/research.html.twig', [
-            'Candidats' => $Candidats,
+        return $this->render('candidats/research.html.twig', [
+            'candidats' => $candidats,
         ]);
     }
 
     /**
-     * @Route("Candidat_account", name="Candidat_account")
+     * @Route("candidat_account", name="candidat_account")
      */
     public function monCompte(UserInterface $Candidat)
     {
         $Candidat = $this->getUser();
-        return $this->render('Candidat/moncompte.html.twig', [
+        return $this->render('candidats/moncompte.html.twig', [
             'Candidat' => $Candidat,
         ]);
     }
 
     /**
-     * @Route("Candidat_account/change_password", name="Candidat_change_password")
+     * @Route("candidat_account/change_password", name="candidat_change_password")
      */
     public function changePassword(UserInterface $Candidat, Request $request, ObjectManager $em, UserPasswordEncoderInterface $encoder)
     {
@@ -284,7 +280,7 @@ class CandidatsController extends AbstractController
                 $em->persist($Candidat);
                 $em->flush();
                 $this->addFlash('mdp_change','Votre mot de passe a été modifié avec succès');
-                return $this->redirectToRoute('Candidat_account');
+                return $this->redirectToRoute('candidat_account');
             }
             else {
                 $mdpNonChange = "Le mot de passe entré n'est pas votre mot de passe actuel";
@@ -293,7 +289,7 @@ class CandidatsController extends AbstractController
 
 
 
-        return $this->render('Candidat/changepassword.html.twig', [
+        return $this->render('candidats/changepassword.html.twig', [
             'Candidat' => $Candidat,
             'form_change_password' => $form->createView(),
             'error' => $mdpNonChange,
@@ -303,7 +299,7 @@ class CandidatsController extends AbstractController
 
 
     /**
-     * @Route("Candidat_account/change_mail", name="change_mail")
+     * @Route("candidat_account/change_mail", name="change_mail")
      */
     public function changeMail(UserInterface $Candidat, Request $request, ObjectManager $em)
     {
@@ -323,60 +319,15 @@ class CandidatsController extends AbstractController
             $em->persist($Candidat);
             $em->flush();
             $this->addFlash('mail_change','Votre mail a été modifié avec succès');
-            return $this->redirectToRoute('Candidat_account');
+            return $this->redirectToRoute('candidat_account');
 
         }
 
-        return $this->render('Candidat/changeemail.html.twig', [
+        return $this->render('candidats/changeemail.html.twig', [
             'Candidat' => $Candidat,
             'form_change_email' => $form->createView()
         ]);
     }
-
-
-
-    /**
-     * @Route("/Candidat/importCsv",name="etu_importCsv")
-     *
-     */
-    public function importCsv(UserInterface $profResp,Candidatsrepository $repoE, ObjectManager $em, UserPasswordEncoderInterface $encoder, ProfesseursRepository $repoP, SecretaireRepository $repoS)
-    {
-        $classe=$this->getUser()->getClasseResponsable();
-        if(isset($_POST['sub'])){
-            $file=$_FILES['importEtu'];
-            $csv = Reader::createFromPath($file['tmp_name'], 'r');
-            $csv->skipEmptyRecords();
-            $csv->setHeaderOffset(0); //set the CSV header offset
-            if(empty($csv->getHeader()) ||count($csv)==0) {
-                $this->addFlash('error','Erreur lors du chargement du fichier');
-                $this->addFlash('info','Veuillez respecter la syntaxe : nom, prenom, mdp, mail, date');
-                return $this->redirectToRoute("research_Candidat");
-            }
-            foreach ($csv as $row)
-            {
-                try
-                {
-                    $this->createCandidat($row['nom_Candidat'],$row['prenom_Candidat'],$row['mdp_Candidat'],$row['mail_Candidat'],
-                        new DateTime($row['date_naissance']),$repoE,$em,$encoder,$repoP,$repoS);
-                }
-                catch (\Exception $errorException)
-                {
-                    $this->addFlash('error','Erreur lors du chargement du fichier');
-                    $this->addFlash('info','Veuillez respecter la syntaxe : nom, prenom, mdp, mail, date');
-                    return $this->redirectToRoute("research_Candidat");
-                }
-            }
-            $this->addFlash('success','La liste de '.count($csv).' étudiant(s) a bien été importée');
-            return $this->redirectToRoute("research_Candidat");
-        }
-        else
-        {
-            return $this->render("index.html.twig");
-        }
-
-
-    }
-
 
     public function createCandidat($nomCandidat,$prenomCandidat,$mdpCandidat,$mail,$date, Candidatsrepository $repoE, ObjectManager $em, UserPasswordEncoderInterface $encoder, ProfesseursRepository $repoP, SecretaireRepository $repoS){
 
