66a135ea563105bd9dd5c5df79f22de770452079
florian1105 <florian110599@gmail.com>
2020-01-30 12:41:10 +0100

    mise a jour des chemin vers public/build

--- siteDesLP/webpack.config.js
+++ siteDesLP/webpack.config.js
@@ -11,13 +11,14 @@ Encore
     // directory where compiled assets will be stored
     .setOutputPath('public/build/')
     // public path used by the web server to access the output path
-    .setPublicPath('build')
+    .setPublicPath('/build')
     // only needed for CDN's or sub-directory deploy
     //.setManifestKeyPrefix('build/')
 
 
     .addPlugin(new CopyWebpackPlugin([
-        { from: './assets/images', to: 'images' }
+        { from: './assets/images', to: 'images'},
+        { from : './assets', to :''}
     ]))
     /*
      * ENTRY CONFIG
@@ -35,6 +36,7 @@ Encore
     .addEntry('treeStructure', './assets/js/treeStructure.js')
     .addEntry('gestionCours', './assets/js/gestionCours.js')
     .addEntry('alert', './assets/js/alert.js')
+
 
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
