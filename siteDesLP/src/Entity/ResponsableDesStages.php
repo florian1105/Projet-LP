36efee803135c6c6403563a3147829d257e4210b
florian1105 <florian110599@gmail.com>
2020-02-26 14:57:55 +0100

    modif responsable des stages

--- siteDesLP/src/Entity/ResponsableDesStages.php
+++ siteDesLP/src/Entity/ResponsableDesStages.php
@@ -18,131 +18,16 @@ class ResponsableDesStages implements UserInterface
      */
     private $id;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $nom;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\OneToOne(targetEntity="App\Entity\Professeurs", cascade={"persist", "remove"})
+     * @ORM\JoinColumn(nullable=false)
      */
-    private $prenom;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $login;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $password;
-
-    /**
-     * @Assert\Length(max = 64, min = 6, minMessage = "Mot de passe trop court, veuillez saisir un mot de passe d'au moins {{ limit }} caractères", maxMessage="Mot de passe trop long il est impossible d'avoir un mot de passe supérieur à {{ limit }} caractères")
-     * @Assert\Regex(pattern="/[☺☻♥♦♣♠•◘○◙♂♀♪♫☼►◄↕‼¶§▬↨↑↓→←∟↔▲▼]/", match=false, message="les caractéres spéciaux ne sont pas autorisés")
-     */
-    private $new_password;
-
-    /**
-     * @Assert\EqualTo(propertyPath="new_password", message="Vous n'avez pas tapé le même mot de passe !")
-     */
-    private $confirmPassword;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $mailAcademique;
+    private $idProfesseur;
 
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getNom(): ?string
-    {
-        return $this->nom;
-    }
-
-    public function setNom(string $nom): self
-    {
-        $this->nom = $nom;
-
-        return $this;
-    }
-
-    public function getPrenom(): ?string
-    {
-        return $this->prenom;
-    }
-
-    public function setPrenom(string $prenom): self
-    {
-        $this->prenom = $prenom;
-
-        return $this;
-    }
-
-    public function getLogin(): ?string
-    {
-        return $this->login;
-    }
-
-    public function setLogin(string $login): self
-    {
-        $this->login = $login;
-
-        return $this;
-    }
-
-    public function getPassword(): ?string
-    {
-        return $this->password;
-    }
-
-    public function setPassword(string $password): self
-    {
-        $this->password = $password;
-        $this->new_password = "XXXXXX";
-        $this->confirm_password = "XXXXXX";
-
-        return $this;
-    }
-
-    public function getNewPassword(): ?string
-    {
-        return $this->new_password;
-    }
-
-    public function setNewPassword(string $new_password): self
-    {
-        $this->new_password = $new_password;
-
-        return $this;
-    }
-
-    public function getConfirmPassword(): ?string
-    {
-        return $this->confirmPassword;
-    }
-
-    public function setConfirmPassword(string $confirmPassword): self
-    {
-        $this->confirmPassword = $confirmPassword;
-
-        return $this;
-    }
-
-    public function getMailAcademique(): ?string
-    {
-        return $this->mailAcademique;
-    }
-
-    public function setMailAcademique(string $mailAcademique): self
-    {
-        $this->mailAcademique = $mailAcademique;
-
-        return $this;
     }
 
     public function eraseCredentials()
@@ -160,13 +45,32 @@ class ResponsableDesStages implements UserInterface
         return ['ROLE_RESPONSABLE_DES_STAGES'];
     }
 
-    public function getUsername()
+
+    public function getIdProfesseur(): ?Professeurs
     {
-        return $this->login;
+        return $this->idProfesseur;
     }
 
-    public function __toString()
+    public function setIdProfesseur(Professeurs $idProfesseur): self
     {
-        return $this->nom." ".$this->prenom;
+        $this->idProfesseur = $idProfesseur;
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getPassword()
+    {
+        // TODO: Implement getPassword() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getUsername()
+    {
+        // TODO: Implement getUsername() method.
     }
 }
